d2047a7201a7c6702fabea044d502778909dc0bf
Gibmyx <gibmyxjobs@gmail.com>
2022-07-24 16:04:06 -0400

    details

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:api'])->get('/user', function (Request $request) {
     return $request->user();
 });
-require __DIR__.'/auth.php';
+require __DIR__.'/api/auth.php';
